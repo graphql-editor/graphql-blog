408a457e77dfeadfd10995985f16ac3bc4880197
Tomek <tomasz@aexol.com>
2020-01-29 11:52:22 +0000

    matt graff gql 2020

---
+++ src/components/authors/Matt/index.js
@@ -0,0 +1,9 @@
+export const Matt = {
+  photo: require('./Matt.jpg'),
+  desc:
+    'Heavy machines, GraphQL passionate.',
+  name: 'Matt Graff',
+  email: 'matt@graphqleditor.com',
+  // github: 'matt-graff',
+  //twitter: 'mattgraff',
+}
