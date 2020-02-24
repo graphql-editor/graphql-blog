d0134792282762871d9bf09bba60e66009d3c3ae
Tomek <tomasz@aexol.com>
2020-02-24 15:07:50 +0000

    profile update

--- src/components/authors/Tomek/index.js
+++ src/components/authors/Tomek/index.js
@@ -1,6 +1,6 @@
 export const Tomek = {
   photo: require('./tomekProfile.jpg'),
-  desc: 'Marketing. Growth Hacking. Magic the Gathering fanatic. Pizza Lover',
+  desc: 'GraphQL enthusiast. Magic the Gathering fanatic. Pizza monster.',
   name: 'Tomek Poniatowicz',
   email: 'tomek@graphqleditor.com',
   twitter: 'TomPoniat',
